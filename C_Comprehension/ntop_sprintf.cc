538e857d92300fbad7f0656d375ac47c17f7382f
DongLai <Dare0012@163.com>
2022-10-22 15:07:16 +0800

    new file:   ntop_sprintf.cc 	modified:   ../Wrapper/wrapper.h

---
+++ C_Comprehension/ntop_sprintf.cc
@@ -0,0 +1,21 @@
+/*
+ * the type of x is "int", which occupies 4 Bytes
+ * so when &x is transfer for char* p
+ * every element of p will occupy 1 Byte, and accessing it is equal to accessingevery 8-bits x
+ */
+
+#include <arpa/inet.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#define MAXLEN 1024
+
+int main(int argc, char *argv[]){
+	assert(argc == 2);
+	
+	int x = atoi(argv[1]);
+	char *p = (char *)&x;
+	char str[MAXLEN];
+	sprintf(str, "%d.%d.%d.%d", p[0], p[1],p[2],p[3]);
+	printf("%s\n", str);
+}
--- Wrapper/wrapper.h
+++ Wrapper/wrapper.h
@@ -6,10 +6,15 @@
 #include <arpa/inet.h>	//socklen_t
 #include <sys/socket.h>	//socket(), bind(), listen(), accept(), connect()
 #include <unistd.h>		//close()
-
+#include <fcntl.h>	//fcntl()
 #define MAXLEN 1024
 
 void unix_eror(const char*);
+void app_error(const char*);
+
+const char *Inet_ntop(int family, const void *src, char *dst, size_t len);
+void Inet_pton(int family, const char *src, void *dst);
+
 int Socket(int domain, int type, int protocol);
 void Bind(int sockfd, struct sockaddr*, socklen_t len);
 void Listen(int servfd, int blocklog);
@@ -17,12 +22,37 @@ int Accept(int servfd, struct cliaddr*, socklen_t &clilen);
 int Connect(int servfd, struct servaddr*, socklen_t &servlen);
 void Close(int sockfd);
 
+int getSendBuf(int sockfd);
+void Set_NONBLOCK(int sockfd);
+void Set_REUSEADDR(int sockfd);
 
 void unix_error(const char *msg){
 	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 	exit(0);
 }
 
+void app_error(const char *msg) {
+	fprintf(stderr, "%s\n", msg);
+	exit(0);
+}
+
+
+const char* Inet_ntop(int family, const void *src, char *dst, size_t size) {
+	const char *ret = inet_ntop(family, src, dst, size);
+
+	if ( ret == NULL)
+		unix_error("inet_ntop() error");
+	return ret;
+}
+
+void Inet_pton(int family, const char *src, void *dst) {
+	int rc;
+
+	if ( (rc = inet_pton(family, src, dst)) == 0)
+		app_error("inet_pton error: invaild dotted-decimal address");
+	else if (rc < 0)
+		unix_error("inet_pton() error");
+}
 int Socket(int domain, int type, int protocol){
 	int sockfd;
 
@@ -65,3 +95,30 @@ void Close(int sockfd){
 		unix_error("close() error");
 }
 
+int getSendBuf(int sockfd) {
+	int sendbuf;
+	socklen_t optlen = sizeof(sendbuf);
+
+	if (getsockopt(sockfd, SOL_SOCKET, SO_SNDBUF, &sendbuf, &optlen) < 0)
+		unix_error("getsockopt() error");
+	return sendbuf;
+}
+
+void Set_NONBLOCK(int sockfd) {
+	int flags;
+	
+	/* set sockfd as nonblocking */
+	if ( (flags = fcntl(sockfd, F_GETFL, 0)) < 0)
+		unix_error("fcntl() GET error");
+	flags |= O_NONBLOCK;
+	if (fcntl(sockfd, F_SETFL, flags) < 0)
+		unix_error("fcntl() SET error");
+}
+
+void Set_REUSEADDR(int sockfd) {
+	int optval = 1;	//not zero: turn on the option
+	socklen_t optlen = sizeof(optval);
+
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&optval, optlen) < 0)
+		unix_error("setsockopt() error");
+}
